1ea15b1cbb740523c46a1d53fb9220044a38e341
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-01-05 15:20:51 +0300

    add moving logic to rook

--- src/models/figures/rook.ts
+++ src/models/figures/rook.ts
@@ -2,11 +2,38 @@ import { Figure, TFigureProps, FIGURE_NAME } from "./figure";
 import { COLORS } from "../color";
 import blackLogo from "../../assets/black-rook.png";
 import whiteLogo from "../../assets/white-rook.png";
+import { Cell } from "../cell";
 
 export class Rook extends Figure {
   constructor({ color, cell }: TFigureProps) {
     super({ color, cell });
     this.logo = color === COLORS.BLACK ? blackLogo : whiteLogo;
     this.name = FIGURE_NAME.ROOK;
+  }
+
+  getIsFugureCanMoveToTarget(target: Cell): boolean {
+    const isTargetIsTheSameColorOrKing = !super.getIsFugureCanMoveToTarget(
+      target
+    );
+
+    if (isTargetIsTheSameColorOrKing) {
+      return false;
+    }
+
+    const isCanMoveToTargetByVerticalLine =
+      this.cell.getIsEmptyVerticalLine(target);
+
+    const isCanMoveToTargetByHorizontalLine =
+      this.cell.getIsEmptyHorisontalLine(target);
+
+    if (isCanMoveToTargetByVerticalLine) {
+      return true;
+    }
+
+    if (isCanMoveToTargetByHorizontalLine) {
+      return true;
+    }
+
+    return false;
   }
 }
