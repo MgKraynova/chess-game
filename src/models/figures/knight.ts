524009544eec9ec471b080f1bb29d719112a661c
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-01-05 15:38:24 +0300

    add moving logic to knight

--- src/models/figures/knight.ts
+++ src/models/figures/knight.ts
@@ -2,11 +2,28 @@ import { Figure, TFigureProps, FIGURE_NAME } from "./figure";
 import { COLORS } from "../color";
 import blackLogo from "../../assets/black-knight.png";
 import whiteLogo from "../../assets/white-knight.png";
+import { Cell } from "../cell";
 
 export class Knight extends Figure {
   constructor({ color, cell }: TFigureProps) {
     super({ color, cell });
     this.logo = color === COLORS.BLACK ? blackLogo : whiteLogo;
     this.name = FIGURE_NAME.KNIGHT;
+  }
+
+  getIsFugureCanMoveToTarget(target: Cell): boolean {
+    const isTargetIsTheSameColorOrKing = !super.getIsFugureCanMoveToTarget(
+      target
+    );
+
+    if (isTargetIsTheSameColorOrKing) {
+      return false;
+    }
+
+    // у коня всегда смещение на 1 по одной координате и на 2 по другой
+    const absX = Math.abs(this.cell.x - target.x);
+    const absY = Math.abs(this.cell.y - target.y);
+
+    return (absX === 1 && absY === 2) || (absX === 2 && absY === 1);
   }
 }
